84e8a5c8448b207d99e45bfd505eae99dd2a03a2
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-07-09 18:19:33 +0900

    ページ遷移するapiを実装しました。

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,50 @@ const PORT = 8080;
 app.use(express.json());
 app.use(cors());
 
-app.get("/getFloorData", async (req: Request, res: Response) => {
-  try {
-    const allRoom = await prisma.floor2.findMany({orderBy: {
-      id: 'asc'
-    }});
-    return res.json(allRoom);
-  } catch (err) {
-    console.log(err);
+const getFloorData = async (floorNumber: string) => {
+  switch (floorNumber) {
+    case '2':
+      return prisma.floor2.findMany({ orderBy: { id: 'asc' } });
+    case '3':
+      return prisma.floor3.findMany({ orderBy: { id: 'asc' } });
+    case '4':
+      return prisma.floor4.findMany({ orderBy: { id: 'asc' } });
+    case '5':
+      return prisma.floor5.findMany({ orderBy: { id: 'asc' } });
+    case '6':
+      return prisma.floor6.findMany({ orderBy: { id: 'asc' } });
+    case '7':
+      return prisma.floor7.findMany({ orderBy: { id: 'asc' } });
+    case '8':
+      return prisma.floor8.findMany({ orderBy: { id: 'asc' } });
+    case '9':
+      return prisma.floor9.findMany({ orderBy: { id: 'asc' } });
+    case '10':
+      return prisma.floor10.findMany({ orderBy: { id: 'asc' } });
+    case '11':
+      return prisma.floor11.findMany({ orderBy: { id: 'asc' } });
+    case '12':
+      return prisma.floor12.findMany({ orderBy: { id: 'asc' } });
+    case '13':
+      return prisma.floor13.findMany({ orderBy: { id: 'asc' } });
+    case '14':
+      return prisma.floor14.findMany({ orderBy: { id: 'asc' } });
+    default:
+      throw new Error(`Invalid floor number: ${floorNumber}`);
   }
-});
+};
 
-app.put("/editRoomState/:id", async (req: Request, res: Response) => {
+app.get('/getFloorData/:floor', async (req: Request, res: Response) => {
+  const { floor } = req.params;
+  try {
+    const FloorData = await getFloorData(floor);
+    return res.json(FloorData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+
+});app.put("/editRoomState/:id", async (req: Request, res: Response) => {
   try {
     const id = Number(req.params.id);
     const { roomState } = req.body;
@@ -30,7 +62,7 @@ app.put("/editRoomState/:id", async (req: Request, res: Response) => {
         roomState,
       },
     });
-    return res.status(201).json("部屋の状態が更新されました")
+    return res.status(201).json("部屋の状態が更新されました");
   } catch (err) {
     return res.status(401).json(err);
   }
