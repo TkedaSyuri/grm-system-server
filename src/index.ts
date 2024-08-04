89373a23d635bbe9e78d2424fbc12ce8460add38
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-08-04 21:50:15 +0900

    本番環境のフロントのurlを追加

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import prisma from "./lib/prismaClient";
 import type { Express, Request, Response } from "express";
 import cors from "cors";
 import http from "http";
-import { Server, Socket } from "socket.io";
+import { Server} from "socket.io";
 
 const app: Express = express();
 const PORT = process.env.PORT || 10000;
 const server: http.Server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://gest-room-moniter-3z2jwbuqy-tkedasyuris-projects.vercel.app/"],
   },
 });
 
