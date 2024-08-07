c7f498e9e4b4fd6f56a0e500200f73fdb53339ae
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-08-07 18:34:40 +0900

    修正

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const server: http.Server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGINS,
     methods: ["GET", "POST"],
   },
 });
